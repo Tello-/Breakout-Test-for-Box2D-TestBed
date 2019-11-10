47b5463be8a836b738cdcd58defc25d3982b036f
Tello- <lollisjosh@gmail.com>
2019-11-09 16:31:31 -0800

    .

--- BreakoutTest.h
+++ BreakoutTest.h
@@ -11,22 +11,9 @@ public:
 	{
 		m_world->SetGravity(b2Vec2{ 0.f,0.f });
 
-		createWall(b2Vec2{ m_worldSize.x,1 }, b2Vec2{ 0,-m_worldSize.y }, 0); // floor (s/p/a)
-		createWall(b2Vec2{ 1,m_worldSize.y }, b2Vec2{ -m_worldSize.x,0 }, M_PI); // left wall
-		createWall(b2Vec2{ 1,m_worldSize.y }, b2Vec2{ m_worldSize.x,0 }, M_PI); // right wall
-		createWall(b2Vec2{ m_worldSize.x,1 }, b2Vec2{ 0,m_worldSize.y }, 0); // top wall
+		createEnclosure(m_worldSize);
 
 		createBall(PTM_RATIO(30.f), b2Vec2{ 0.f,0.f }, 0.f);
-	}
-
-	void Step(Settings* settings)
-	{
-		//run the default physics and rendering
-		Test::Step(settings);
-
-		//show some text in the main screen
-		g_debugDraw.DrawString(5, m_textLine, "Now we have a Breakout test");
-		m_textLine += 15;
 	}
 
 	static Test* Create()
@@ -35,45 +22,11 @@ public:
 	}
 
 private:
+	void createEnclosure(b2Vec2 size);
+	void createWall(b2Vec2 size, b2Vec2 pos, float32 angle);
+	void createBall(float radius, b2Vec2 pos, float32 angle);
 
-	void createWall(b2Vec2 size, b2Vec2 pos, float32 angle)
-	{
-		b2BodyDef bodyDef;
-		bodyDef.type = b2_staticBody;
-		bodyDef.position.Set(pos.x, pos.y);
-		bodyDef.angle = angle;
-
-		b2Body* body = m_world->CreateBody(&bodyDef);
-
-		b2PolygonShape shape;
-		shape.SetAsBox(size.x, size.y);
-
-		b2FixtureDef fixtureDef;
-		fixtureDef.shape = &shape;
-		fixtureDef.density = 1.f;
-		body->CreateFixture(&fixtureDef);
-	}
-
-	void createBall(float radius, b2Vec2 pos, float32 angle)
-	{
-		b2BodyDef myBodyDef;
-		myBodyDef.type = b2_dynamicBody; //this will be a dynamic body
-		myBodyDef.position.Set(pos.x,pos.y); //set the starting position
-		myBodyDef.angle = angle; //set the starting angle
-
-		b2Body* dynamicBody = m_world->CreateBody(&myBodyDef);
-
-		b2CircleShape circShape;
-		circShape.m_radius = radius;
-
-		b2FixtureDef circFixtureDef;
-
-		circFixtureDef.shape = &circShape;
-		circFixtureDef.density = 0.f;
-		circFixtureDef.restitution = 1.f;
-		circFixtureDef.friction = 0.f;
-		dynamicBody->CreateFixture(&circFixtureDef);
-	}
+	
 
 	// 30 px / 1 meter
 	float32 PTM_RATIO(float32 pixels) { return pixels * 0.0333f; }
@@ -90,3 +43,65 @@ private:
 };
 
 #endif
+
+void BreakoutTest::createEnclosure(b2Vec2 size)
+{
+	b2BodyDef bd;
+	b2Body* ground = m_world->CreateBody(&bd);
+
+	b2EdgeShape shape;
+
+	// Floor
+	shape.Set(b2Vec2(-size.x, 0.0f), b2Vec2(size.x, 0.0f));
+	ground->CreateFixture(&shape, 0.0f);
+
+	// Left wall
+	shape.Set(b2Vec2(-size.x, 0.0f), b2Vec2(-size.x, size.y * 2));
+	ground->CreateFixture(&shape, 0.0f);
+
+	// Right wall
+	shape.Set(b2Vec2(size.x, 0.0f), b2Vec2(size.x, size.y * 2));
+	ground->CreateFixture(&shape, 0.0f);
+
+	// Roof
+	shape.Set(b2Vec2(-size.x, size.y * 2), b2Vec2(size.x, size.y * 2));
+	ground->CreateFixture(&shape, 0.0f);
+}
+void BreakoutTest::createWall(b2Vec2 size, b2Vec2 pos, float32 angle)
+{
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_staticBody;
+	bodyDef.position.Set(pos.x, pos.y);
+	bodyDef.angle = angle;
+
+	b2Body* body = m_world->CreateBody(&bodyDef);
+
+	b2PolygonShape shape;
+	shape.SetAsBox(size.x, size.y);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &shape;
+	fixtureDef.density = 1.f;
+	body->CreateFixture(&fixtureDef);
+}
+
+void BreakoutTest::createBall(float radius, b2Vec2 pos, float32 angle)
+{
+	b2BodyDef myBodyDef;
+	myBodyDef.type = b2_dynamicBody; //this will be a dynamic body
+	myBodyDef.position.Set(pos.x, pos.y); //set the starting position
+	myBodyDef.angle = angle; //set the starting angle
+
+	b2Body* dynamicBody = m_world->CreateBody(&myBodyDef);
+
+	b2CircleShape circShape;
+	circShape.m_radius = radius;
+
+	b2FixtureDef circFixtureDef;
+
+	circFixtureDef.shape = &circShape;
+	circFixtureDef.density = 0.f;
+	circFixtureDef.restitution = 1.f;
+	circFixtureDef.friction = 0.f;
+	dynamicBody->CreateFixture(&circFixtureDef);
+}
